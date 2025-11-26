cd50e228549d49a758575841c62d81b6a7c27715
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2025-11-26 15:50:14 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ricing SWAY WM
+
+#### Config files for:
+
+- `sway`
+- `foot`
+
+### Installation
+
+- Clone this repository -
+```
+$ git clone --depth=1 https://github.com/AlessandroPerazzetta/ricing-sway.git
+```
+
+- Change to cloned directory and make setup.sh executable -
+```
+$ cd ricing-sway
+$ chmod +x setup.sh
+```
+
+- Run `setup.sh` -
+```
+$ ./setup.sh
---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Install script for Sway ricing
+
+# Define source and target directories
+SRC_DIR="$(pwd)"
+TARGET_DIR="$HOME/.config"
+BACKUP_DIR="$HOME/config_backup_$(date +%Y%m%d_%H%M%S)"
+# List of config directories to symlink
+CONFIG_DIRS=("sway" "foot")
+# Create backup directory
+mkdir -p "$BACKUP_DIR"
+
+echo "Backup directory created at $BACKUP_DIR"
+tar -czf "$BACKUP_DIR/configs_backup.tar.gz" -C "$TARGET_DIR" "${CONFIG_DIRS[@]}"
+echo "Existing configurations backed up to $BACKUP_DIR/configs_backup.tar.gz"
+
+# Copy new config from source to target
+for dir in "${CONFIG_DIRS[@]}"; do
+    SRC_PATH="$SRC_DIR/$dir"
+    TARGET_PATH="$TARGET_DIR/$dir"
+    if [ -d "$SRC_PATH" ]; then
+        # Remove existing config if it exists
+        if [ -e "$TARGET_PATH" ] || [ -L "$TARGET_PATH" ]; then
+            rm -rf "$TARGET_PATH"
+            echo "Removed existing configuration at $TARGET_PATH"
+        fi
+        # Copy new config
+        cp -r "$SRC_PATH" "$TARGET_PATH"
+        echo "Copied $SRC_PATH to $TARGET_PATH"
+    else
+        echo "Source directory $SRC_PATH does not exist. Skipping."
+    fi
+done
+
+
+# Install necessary packages
+echo "Installing necessary packages..."
+
+# List of pacman packages to install
+pacman_packages=(
+    "dmenu"
+    "ttf-nerd-fonts-symbols"
+    "ttf-jetbrains-mono-nerd"
+)
+
+# Update system and install packages
+echo "Updating system..."
+sudo pacman -Syu --noconfirm
+
+echo "Installing pacman packages..."
+for package in "${pacman_packages[@]}"; do
+    echo "Installing $package..."
+    sudo pacman -S --noconfirm "$package"
+done
